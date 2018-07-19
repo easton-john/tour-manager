aa5269dce104a5e3d85cf3523d1ddf4cc3e15129
Easton Gorishek <ieaston26@gmail.com>
2018-07-18 17:31:20 -0700

    validates a good model - done

---
+++ test/unit/tour.test.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+const Tour = require('../../lib/models/tour');
+
+describe('Circus Tour API', () => {
+
+    it('validates a good model', () => {
+        const data = {
+            title: 'Flying Monkeys',
+            activities: ['Monkeys playing instruments and such'],
+            launchDate: new Date(2018, 8, 22),
+            stops: {
+                location: {
+                    city: 'Portland',
+                    state: 'Oregon',
+                    zip: 94770
+                },
+                weather: {
+                    temperature: 80,
+                    condition: 'Summertime heat'
+                },
+                attendance: 20
+            }
+        };
+
+        const tour = new Tour(data);
+        const json = tour.toJSON();
+        delete json._id;
+        assert.deepEqual(json, data);
+
+    });
+});
